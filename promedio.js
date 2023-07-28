72516a8ef02b940ef6f86212a2239d0bf496157d
���Valeria <arenasgarciavaleria3@gmail.com>
2023-07-28 11:29:26 -0500

    Final version

--- promedio.js
+++ promedio.js
@@ -1,2 +1,6 @@
 let num1=Number(prompt("Ingresa el primer numero"))
 let num2=Number(prompt("Ingresa el segundo numero"))
+let suma=num1+num2
+let prom=suma/2
+
+alert(`El promedio de los 2 numeros es ${prom}`)
